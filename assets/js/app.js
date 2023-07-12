ca15e7f6371077670a5e60f64dfb47b587191e65
Mahesh Babu <mahesh.b.pec@gmail.com>
2023-07-12 16:09:36 +0530

    Added blog page

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,7 +23,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="reference.html">References</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="event.html">Events</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://medium.com/@mahesh.babu11" target="_blank">Blogs</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="blog.html">Blog</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()">
    </li>
